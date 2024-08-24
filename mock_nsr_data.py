fef952d64e66a45d04978eea198d1c81ecd9af4d
Matheus Barros <matheuspereirabarros@ufpi.edu.br>
2024-08-24 16:27:45 -0300

    Atualização do script de geração de dados

--- mock_nsr_data.py
+++ mock_nsr_data.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import numpy as np
-from faker import Faker
 import random
 from datetime import datetime, timedelta
 
 # Configurações iniciais
-fake = Faker()
-Faker.seed(42)
 random.seed(42)
 np.random.seed(42)
 
